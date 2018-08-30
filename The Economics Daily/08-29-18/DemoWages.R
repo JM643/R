f180d0438167f374c55c70ac99e529e6181a126c
Jason M <40440123+JM643@users.noreply.github.com>
2018-08-29 20:10:02 -0400

    Update DemoWages.R

--- The Economics Daily/08-29-18/DemoWages.R
+++ The Economics Daily/08-29-18/DemoWages.R
@@ -12,6 +12,7 @@ barplot(demo_data, ylim = c(0, 1600), names.arg = NULL, beside = TRUE, col = c("
         main = "Median usual weekly earnings of women and men who are full-time\n wage and salary workers, by race and Hispanic or Latino ethnicity, 2017")
 # add column names 
 colnames(demo_data) <- c("Total", "White", "Black", "Asian", "Hispanic")
+# bold legend text
 par(font=2)
 legend("top", c("Women","Men"), horiz = TRUE, text.width = 0.9, bty = "n", inset = -.02, cex = 0.95, fill = c("purple","orange"))
 
