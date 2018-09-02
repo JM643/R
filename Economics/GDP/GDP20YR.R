dddd7d33834c06ec0b3ed08057ee1911dbe8d208
Jason M <40440123+JM643@users.noreply.github.com>
2018-09-02 13:11:56 -0400

    Update GDP20YR.R

--- Economics/GDP/GDP20YR.R
+++ Economics/GDP/GDP20YR.R
@@ -1,4 +1,4 @@
-# Read GDP over the last 20 years, 1998-2018
+# Real GDP Growth, 1998-2018
 # Data taken from St. Louis FRED
 # https://fred.stlouisfed.org/series/A191RL1Q225SBEA
 
@@ -6,7 +6,8 @@ Year <- (GDP20YR$observation_date)
 GDP <- (GDP20YR$GDP)
 
 plot(Year, GDP, "l", 
-     col = "blue", 
-     main = "Real GDP over the last 20 years, 1998-2018",
-     lwd =3, ylim = c(-9,9))
-     abline(h = 0)
+     col = "darkblue", 
+     main = "Real GDP Growth %, 1998-2018",
+     lwd =3,las = 1, ylim = c(-15,15))#las = 1, )
+grid(NA, 5, lwd = 1, lty = "solid", col = "lightgray")
+abline(h = 0)
