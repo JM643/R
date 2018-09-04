e851d20aeb6b530e5567d972e5b3ad6d923aab03
Jason M <40440123+JM643@users.noreply.github.com>
2018-09-04 17:59:59 -0400

    Update PSAVERT.R

--- Economics/Personal Savings/PSAVERT.R
+++ Economics/Personal Savings/PSAVERT.R
@@ -1,11 +1,13 @@
 #Personal Savings Rate, 1960-2018
 #https://fred.stlouisfed.org/series/PSAVERT#0
+# PSAVERT.xls
 
 library(ggplot2)
 theme_set(theme_bw())
 
 ggplot() + 
   geom_line(aes(x = obdate, y = PSAVERT), colour = "dodgerblue", data = PSAVERT) + 
+#Sheet2 - RECESSION
   geom_rect(data = PSAVERT2, aes(xmin = x1, xmax = x2, ymin = -Inf, ymax = Inf), fill = 'gray', alpha = 0.20) + 
   labs(title = "Personal Savings Rate, 1960-2018", 
        subtitle = "Seasonal Adjusted, Annual Rate | Frequency: Monthly",  
