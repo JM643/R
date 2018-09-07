575c96d5bb98a2e0b14e5b5de3d265c563b7481e
Jason M <40440123+JM643@users.noreply.github.com>
2018-09-07 16:04:10 -0400

    Update SchEmply.R

--- The Economics Daily/09-06-18/SchEmply.R
+++ The Economics Daily/09-06-18/SchEmply.R
@@ -2,18 +2,19 @@
 # https://www.bls.gov/opub/ted/2018/a-look-at-elementary-and-secondary-school-employment.htm#bls-print
 
 library (ggplot2)
+library(scales)
 theme_set(theme_bw())
 
 ggplot(data = SchoolEmply, aes(Occupation, Employment)) + 
   geom_bar(data = SchoolEmply, aes(x = reorder(Occupation, Employment)), stat = 'identity', fill = 'darkblue') + 
  coord_flip() +
-  labs(title = "Employment of teachers and instructors in elementary and secondary schools, May 2017",
-       caption = "Source: U.S. Bureau of Labor Statistics") 
+  labs(title = "Employment of teachers and instructors in elementary and secondary\n schools, May 2017",
+       caption = "Source: U.S. Bureau of Labor Statistics",
+       x = " ") + 
+  # remove and manual add  axis ticks 
+  scale_x_discrete(labels = c("Self-enrichment education teachers", "Adult basic and secondary education and\n literacy teachers and instructors", "Instructional coordinators", "Teachers and instructors, all other, except\n substitute teachers", "Preschool and kindergarten teachers", "Special education teachers", "Substitute teachers", "Teacher assistants", "Secondary school teachers", "Elementary and middle school teachers")) + 
+  # add commas to indicate thousands and millions
+  scale_y_continuous(label = comma) 
 
 
 
-
-
-
-
-# still working on how to adjust the y-axis ticks, margins and numbers on x-axis (commas)
