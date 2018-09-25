f885796bbe2093ea720988994c98b05c2733f272
Jason M <40440123+JMEcon@users.noreply.github.com>
2018-09-25 16:21:55 -0400

    Update Latinolabp.R

--- The Economics Daily/09-25-18/Latinolabp.R
+++ The Economics Daily/09-25-18/Latinolabp.R
@@ -18,11 +18,14 @@ latinolabp.df <- data.frame(c(latinolabp[c(10), c(1:2)]))
 # lock in factor so that order appears execatly like it does in data frame when plotted
 latinolabp.df$group <- factor(latinolabp.df$group, levels = latinolabp.df$group)
 
+# new factor for original data frame -- used for scale_x_discrete so that order is in line with orginal data frame
+latinolabp <- factor(latinolabp$group, levels = latinolabp$group)
+
 ggplot() + 
-  geom_bar(data = newlatinolabp, aes(group, part_rate), stat = "identity", fill = "red", width = 0.5) + 
+  geom_bar(data = newlatinolabp, aes(group, part_rate), stat = "identity", fill = "red3", width = 0.5) + 
   geom_bar(data = latinolabp.df, aes(group, part_rate), stat = "identity", fill = "blue", width = 0.5) +
   coord_flip() +
-  scale_x_discrete(limits = rev(levels(latinolabp$group))) + 
+  scale_x_discrete(limits = rev(levels(latinolabp))) +  # rev reverses the order of coord_flip
   scale_y_continuous(labels = scales::percent) + 
   labs(title = "Labor force participation rate of the civilian noninstitutional population by\ndetailed Hispanic or Latino ethnicity, 2017 annual averages",
        x = " ",
