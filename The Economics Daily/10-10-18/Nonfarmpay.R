8d96c6f26471c293196e07aff2abddc9a6638764
Jason M <40440123+JMEcon@users.noreply.github.com>
2018-10-10 18:39:35 -0400

    Create Nonfarmpay.R

---
+++ The Economics Daily/10-10-18/Nonfarmpay.R
@@ -0,0 +1,25 @@
+library(ggplot2)
+theme_set(theme_bw())
+
+# new data frame; removes first row from Excel spreadsheet
+nonfarm <- data.frame(industry = yrnonfarmpay$industry[2:18], n_employ = yrnonfarmpay$year_employ[2:18],
+                      p_employ = yrnonfarmpay$year_perc_employ[2:18])
+nonfarm$industry <- factor(nonfarm$industry, levels = nonfarm$industry)
+
+ggplot(nonfarm, aes(n_employ, p_employ, color = industry)) + 
+  geom_point(aes(color = industry), size = 2.5) + 
+  labs(title = "12-month change in nonfarm payroll employment, by industry, September 2018, seasonally adjusted",
+       x = "12-month change in employment",
+       y = "12-month percent change in employment",
+       caption = "Source: U.S. Bureau of Labor Statistics") + 
+  theme(legend.position = "top") + 
+  theme(legend.text = element_text(face = "bold")) + 
+  theme(legend.title = element_blank()) + 
+  theme(plot.title = element_text(face = "bold")) + 
+  scale_x_continuous(breaks = c(-100000, 0, 100000, 200000, 300000, 400000, 500000),
+                     labels =  c("-100,000", "0", "100,000", "200,000", "300,000", "400,000", "500,000")) + 
+  scale_y_continuous(labels = function(x) paste0(x*100, "%")) + 
+  scale_color_manual(values = c("red", "purple", "darkred", "yellow", "sienna4", "dodgerblue4", "chartreuse4",
+                     "deeppink1", "deeppink4", "turquoise4", "cyan3", "firebrick2", "cyan1", "magenta1", 
+                     "lawngreen", "purple4", "darkblue")) + 
+  ggsave("nonfarmpay.png", width = 11, height = 8.5)
