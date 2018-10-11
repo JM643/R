09436837a4bf4bd43b9d4a8f186a270ae0960eec
Jason M <40440123+JMEcon@users.noreply.github.com>
2018-10-10 21:35:52 -0400

    Update Nonfarmpay.R

--- The Economics Daily/10-10-18/Nonfarmpay.R
+++ The Economics Daily/10-10-18/Nonfarmpay.R
@@ -6,7 +6,7 @@
 library(ggplot2)
 theme_set(theme_bw())
 
-# new data frame; removes first row from Excel spreadsheet
+# new data frame; removes first row and last column from Excel spreadsheet
 nonfarm <- data.frame(industry = nonfarmpay$industry[2:18], n_employ = nonfarmpay$year_employ[2:18],
                       p_employ = nonfarmpay$year_perc_employ[2:18])
 nonfarm$industry <- factor(nonfarm$industry, levels = nonfarm$industry)
@@ -16,6 +16,7 @@ ggplot(nonfarm, aes(n_employ, p_employ, color = industry)) +
   geom_vline(xintercept = 0) +
   geom_hline(yintercept = 0) +
   labs(title = "12-month change in nonfarm payroll employment, by industry, September 2018, seasonally adjusted",
+       subtitle = "* Statistically significant change", 
        x = "12-month change in employment",
        y = "12-month percent change in employment",
        caption = "Source: U.S. Bureau of Labor Statistics") + 
@@ -23,10 +24,16 @@ ggplot(nonfarm, aes(n_employ, p_employ, color = industry)) +
   theme(legend.text = element_text(face = "bold")) + 
   theme(legend.title = element_blank()) + 
   theme(plot.title = element_text(face = "bold")) + 
+  theme(plot.subtitle = element_text(size = 9, vjust = -50)) + 
   scale_x_continuous(breaks = c(-100000, 0, 100000, 200000, 300000, 400000, 500000),
                      labels =  c("-100,000", "0", "100,000", "200,000", "300,000", "400,000", "500,000")) + 
   scale_y_continuous(labels = function(x) paste0(x*100, "%")) + 
   scale_color_manual(values = c("red", "purple", "darkred", "yellow", "sienna4", "dodgerblue4", "chartreuse4",
                      "deeppink1", "deeppink4", "turquoise4", "cyan3", "firebrick2", "cyan1", "magenta1", 
-                     "lawngreen", "purple4", "darkblue")) + 
+                     "lawngreen", "purple4", "darkblue"), 
+                     labels = c("Mining and logging*", "Construction*", "Manufacturing*", "Wholesale trade*", 
+                     "Retail trade", "Transportation and warehousing*", "Utilities", "Information", "Financial activities", 
+                     "Professional and business services*", "Educational services", "Health care and social assistance*", 
+                     "Leisure and hospitality*", "Other services*", "Federal government", "State government", "Local government")) + 
+  guides(col = guide_legend(ncol = 3)) + 
   ggsave("nonfarmpay.png", width = 11, height = 8.5)
